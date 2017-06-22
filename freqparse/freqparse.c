87311ddd2a5613d6ed2e448f4cf19459b5c3ef70
Jacob Bert <jacob_bert@Jacob-Berts-Macbook-Pro.local>
2017-06-22 12:28:45 -0700

    Revision 3

--- freqparse/freqparse.c
+++ freqparse/freqparse.c
@@ -1,31 +1,47 @@
 #include <stdio.h>
 
+void print(char *c){
+  while (*c != '\0'){
+      printf("%c", *c);
+      c++;}
+  printf("\n");}
+
+
+
 
 int main(){
 
 char string[100];
-
+//////////////////
 int i;
 int length = strlen(string);
 int foundLower = 0;
 int foundUpper = 0;
 int foundDigit = 0;
+int foundOther = 0;
+///////////////////
 
-for(i=0; i<30; i++)
- scanf("%f", string[i]);
+printf("Enter a string:\n>");
+scanf("%s", string);
 
 for (i = 0; i < strlen(string); ++i) {
-    if ((string[i] >= 'a') && (string[i] <= 'z')) //check for lower
+    if ((string[i] >= 'a') && (string[i] <= 'z') ) //check for lower
         {foundLower+=1;}
     else if ((string [i] >= 'A') && (string[i] <= 'Z')) //check for upper
         {foundUpper+=1;}
     else if ((string[i] >= '0') && (string[i] <='9'))
         {foundDigit+=1;}
+    else
+        {foundOther+=1;}
       }
 
+printf("\n\n\n");
+print(string);
+printf("length of string = %d\n", length);
 printf("foundLower = %d\n", foundLower);
 printf("foundUpper = %d\n", foundUpper);
 printf("foundDigit = %d\n", foundDigit);
+printf("foundOther = %d\n", foundOther);
 
 
 
